278832cd200456b8b78409c24e2f2161449cc8f8
ceyhun <ceyhunerturk1@gmail.com>
2024-04-08 12:45:19 +0300

    malloc revised

--- malloc.c
+++ malloc.c
@@ -2,22 +2,22 @@
 #include "malloc.h"
 
 extern uint8_t __data_end;
-static uint8_t* bitmap_base_addr = &__data_end;
+static uint8_t* heapmap_base_addr = &__data_end;
 
 static uint8_t* heap_free_base_addr;
 
 static void apply_alignemt(uint16_t* size_in_bytes)
 {
-    if (*size_in_bytes < PER_CELL_SIZE)
+    if (*size_in_bytes < CELL_SIZE)
     {
-        *size_in_bytes = PER_CELL_SIZE;
+        *size_in_bytes = CELL_SIZE;
         return;
     }
 
-    // apply alignment to PER_CELL_SIZE
+    // apply alignment to CELL_SIZE
     {
-        uint16_t remainder = *size_in_bytes % PER_CELL_SIZE;
-        uint16_t adder = PER_CELL_SIZE - remainder;
+        uint16_t remainder = *size_in_bytes % CELL_SIZE;
+        uint16_t adder = CELL_SIZE - remainder;
         *size_in_bytes += adder;
     }
 }
@@ -25,40 +25,40 @@ static void apply_alignemt(uint16_t* size_in_bytes)
 static void* apply_occupy(uint8_t base_idx, uint8_t total_req_cells)
 {
     void* allocated_ptr = heap_free_base_addr;
-    
+
     if (total_req_cells == 1)
     {
-        bitmap_base_addr[base_idx] = BITMAP_CELL_INUSE;
+        heapmap_base_addr[base_idx] = BITMAP_CELL_INUSE;
 
-        heap_free_base_addr += PER_CELL_SIZE;
+        heap_free_base_addr += CELL_SIZE;
         return allocated_ptr;
     }
 
-    bitmap_base_addr[base_idx++] = BITMAP_CELL_INUSE;
+    heapmap_base_addr[base_idx++] = BITMAP_CELL_INUSE;
 
-    while (base_idx < total_req_cells - 1) // last cell will be used for inuse_last
+    while (base_idx < total_req_cells - 1)  // last cell will be used for inuse_last
     {
-        bitmap_base_addr[base_idx++] = BITMAP_CELL_INUSE_CONTUNIE;
+        heapmap_base_addr[base_idx++] = BITMAP_CELL_INUSE_CONTUNIE;
     }
 
-    bitmap_base_addr[base_idx] = BITMAP_CELL_INUSE_LAST;
+    heapmap_base_addr[base_idx] = BITMAP_CELL_INUSE_LAST;
 
-    heap_free_base_addr += (total_req_cells * PER_CELL_SIZE);
+    heap_free_base_addr += (total_req_cells * CELL_SIZE);
     return allocated_ptr;
 }
 
-void malloc_init()
+void heap_init()
 {
-    memset(bitmap_base_addr, BITMAP_CELL_FREE, BITMAP_SIZE);
+    memset(heapmap_base_addr, BITMAP_CELL_FREE, HEAP_CELLS);
 
-    heap_free_base_addr = bitmap_base_addr + (PER_CELL_SIZE * BITMAP_SIZE);
+    heap_free_base_addr = heapmap_base_addr + (CELL_SIZE * HEAP_CELLS);
 }
 
 void* malloc(uint16_t size_in_bytes)
 {
     apply_alignemt(&size_in_bytes);
 
-    uint8_t total_req_cells = size_in_bytes / PER_CELL_SIZE;
+    uint8_t total_req_cells = size_in_bytes / CELL_SIZE;
 
     uint8_t encounted_cells = 0;
     uint8_t found = 0;
@@ -66,11 +66,11 @@ void* malloc(uint16_t size_in_bytes)
 
     uint8_t idx = 0;
 
-    while (idx < BITMAP_SIZE)
+    while (idx < HEAP_CELLS)
     {
         encounted_cells++;
 
-        if (bitmap_base_addr[idx] != BITMAP_CELL_FREE)
+        if (heapmap_base_addr[idx] != BITMAP_CELL_FREE)
         {
             encounted_cells = 0;
             base_idx = idx + 1;
