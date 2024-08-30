a50419742f9d7c30d36cde60670092efd89a08c7
ceyert <98092113+ceyert@users.noreply.github.com>
2024-08-31 01:21:49 +0300

    Comments added to malloc.h

--- malloc.h
+++ malloc.h
@@ -3,19 +3,18 @@
 
 #include "types.h"
 
-#define HEAP_CELLS 140                            // total heapmap size
-#define CELL_SIZE 8                               // 8 bytes each CELL
-#define TOTAL_HEAP_SIZE (HEAP_CELLS * CELL_SIZE)  // 1120 bytes heap in total
+#define HEAP_CELLS 140                            // Total number of cells in the heap map
+#define CELL_SIZE 8                               // Size of each cell in bytes (8 bytes)
+#define TOTAL_HEAP_SIZE (HEAP_CELLS * CELL_SIZE)  // Total heap size in bytes (1120 bytes)
 
-#define CELL_FREE            0b00000000
-#define CELL_INUSE           0b00000001
-#define CELL_INUSE_CONTUNIE  0b00000011
-#define CELL_INUSE_LAST      0b00000111
+#define CELL_FREE            0b00000000           // Cell is free and available for allocation
+#define CELL_INUSE           0b00000001           // Cell is currently in use
+#define CELL_INUSE_CONTUNIE  0b00000011           // Cell is in use and part of a continued block
+#define CELL_INUSE_LAST      0b00000111           // Last cell in a block that is in use
 
-extern void heap_init();
-
-extern void* malloc(uint16_t size);
-
-extern char free(uint8_t* base_addr);
+// API declarations
+extern void heap_init();                          // Initialize the heap
+extern void* malloc(uint16_t size);               // Allocate memory of given size
+extern char free(uint8_t* base_addr);             // Free the allocated memory at the given address
 
 #endif  // MALLOC_H
